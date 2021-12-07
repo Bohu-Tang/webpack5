1f6bce15b05e41900257e531ead001cd1c64ce3a
肖鲁新 <losen_xiao@topsky.com>
2021-12-07 14:43:55 +0800

    zancun

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,60 +1,64 @@
 const path = require("path");
 const {
-  VueLoaderPlugin
+    VueLoaderPlugin
 } = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {
-  join
+    join
 } = require("path");
 
 module.exports = (env) => {
-  return {
-    entry: "./src/main.js", //入口文件
-    stats: 'errors-only', //仅错误时显示logo
-    output: {
-      filename: "assets/js/[name].code.js",
-      chunkFilename: "assets/js/[name].bundle.js", //动态导入 分离bundle 比如lodashjs配合注释import(/* webpackChunkName: "lodash" */ 'lodash') 会打包成lodash.bundle.js
-      path: path.resolve(__dirname, "../dist"),
-      clean: true
-    },
-    plugins: [
-      new VueLoaderPlugin(),
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, "../public/index.html"),
-        filename: "index.html", //输出的文件名
-      }),
-    ],
-    resolve: { //配置模块如会解析
-      extensions: ['.vue', '.js', '.json'], //引入这些文件 可以不带后缀 按顺序解析
-      alias: {
-        '@': join('../src'), //@方式引入资源
-      }
-    },
-    module: {
-      rules: [{
-          test: /\.vue$/,
-          loader: "vue-loader",
-        }, // 它会应用到普通的 `.js` 文件以及 `.vue` 文件中的 `<script>` 块
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
+    return {
+        entry: "./src/main.js", //入口文件
+        stats: 'errors-only', //仅错误时显示logo
+        output: {
+            filename: "assets/js/[name].code.js",
+            chunkFilename: "assets/js/[name].bundle.js", // 动态导入 分离bundle 比如lodashjs配合注释import(/* webpackChunkName: "lodash" */ 'lodash') 会打包成lodash.bundle.js
+            path: path.resolve(__dirname, "../dist"),
+            clean: true
         },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          type: 'asset',
-          parser: {
-            dataUrlCondition: {
-              maxSize: 6 * 1024, //小于6kb的图片内联处理
+        plugins: [
+            //vue-loader插件配置
+            new VueLoaderPlugin(),
+            // htmlWebpackPlugin配置
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "../public/index.html"),
+                filename: "index.html", // 输出的文件名
+                title: 'webpack5搭建vue3项目' // 输出的html文件的title
+            }),
+
+        ],
+        resolve: { //配置模块如会解析
+            extensions: ['.vue', '.js', '.json'], //引入这些文件 可以不带后缀 按顺序解析
+            alias: {
+                '@': join('../src'), //@方式引入资源
             }
-          }
-        }
-      ],
-    },
-  };
+        },
+        module: {
+            rules: [{
+                test: /\.vue$/,
+                loader: "vue-loader",
+            }, // 它会应用到普通的 `.js` 文件以及 `.vue` 文件中的 `<script>` 块
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                        },
+                    },
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    type: 'asset',
+                    parser: {
+                        dataUrlCondition: {
+                            maxSize: 6 * 1024, //小于6kb的图片内联处理
+                        }
+                    }
+                }
+            ],
+        },
+    };
 };
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
-  <div>webpack5搭建vue3教程</div>
   <router-view/>
+  <el-button type="primary">测试引入</el-button>
 </template>
 <script>
 export default {
-  name:'App',
-  components:{
-       
-  }
+  name:'App'
 }
 </script>
-<style>
-
-</style>
